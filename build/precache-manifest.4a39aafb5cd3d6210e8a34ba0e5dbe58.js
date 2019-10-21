7f132749adc0ae1d92d1716535bd1352cbe31f19
manuelgomezo <manuel.gomezo@udea.edu.co>
2019-10-21 14:42:11 -0500

    build folder removed from gitignore

---
+++ build/precache-manifest.4a39aafb5cd3d6210e8a34ba0e5dbe58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d590858086fa3e54d886442a3c762785",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8617bc6104bdd1ca5e6",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f2b5db4edad0515eb1cf",
+    "url": "/static/js/2.507c8dfe.chunk.js"
+  },
+  {
+    "revision": "b8617bc6104bdd1ca5e6",
+    "url": "/static/js/main.210d0e14.chunk.js"
+  },
+  {
+    "revision": "8f2939e1e38cc19f20f3",
+    "url": "/static/js/runtime-main.a2786b06.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
